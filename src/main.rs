6f123aa838de86797e69d04b910d359ef36a227a
Kendric Ng <kendricvaughn_ng@apple.com>
2022-06-18 16:06:08 -0700

    Generate private key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use bdk::{database::MemoryDatabase, electrum_client::Client};
+use bdk::bitcoin::{secp256k1, PrivateKey, Network};
+
+fn generate_private_key() -> PrivateKey {
+    let key = secp256k1::SecretKey::new(&mut secp256k1::rand::thread_rng());
+    let private_key = PrivateKey {
+        compressed: true,
+        network: Network::Testnet,
+        inner: key
+    };
+    private_key
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", generate_private_key());
+    
+    let cache = MemoryDatabase::new();
+    let client = Client::new("ssl://electrum.blockstream.info:60002")?;
+
+    Ok(())
 }
